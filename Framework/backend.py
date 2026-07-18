1bb87057bb75f59f36123ea55cca91f9dfb96281
Milan007 <163461407+Milan070207@users.noreply.github.com>
2026-07-18 18:30:23 +0200

    Implement dynamic backend selection for numpy/cupy
    Add support for selecting backend library based on environment variable.

--- Framework/backend.py
+++ Framework/backend.py
@@ -1,1 +1,19 @@
-import numpy as xp
+import os
+
+if os.environ.get("XP_BACKEND", "").lower() == "numpy":
+    import numpy as xp
+    BACKEND = "numpy"
+
+else:
+    try:
+        import cupy as cp
+
+        if cp.cuda.runtime.getDeviceCount() > 0:
+            xp = cp
+            BACKEND = "cupy"
+        else:
+            raise RuntimeError
+
+    except Exception:
+        import numpy as xp
+        BACKEND = "numpy"
